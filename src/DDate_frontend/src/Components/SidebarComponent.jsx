3d8ff5079658512c2a2dc31ff532f1b45cb41a92
harshtec <harsh.tech@quadbtech.com>
2024-07-18 17:58:25 +0530

    bug Fix in reloadLogin & UserID transfer in every component

--- src/DDate_frontend/src/Components/SidebarComponent.jsx
+++ src/DDate_frontend/src/Components/SidebarComponent.jsx
@@ -1,4 +1,3 @@
-
 import React from "react";
 // import userpic from "../../assets/Images/UserProfiles/userpic.svg";
 import logo from "../../assets/Images/CreateAccount/logo.png";
@@ -9,10 +8,7 @@ import { Principal } from "@dfinity/principal";
 import { IoLogOut } from "react-icons/io5";
 import Loader from "./Loader";
 import { useAuth } from "../auth/useAuthClient";
-import chirag from "../../assets/Images/CreateAccount/chirag.jpg";
-
-
-
+import userpic from "../../assets/Images/UserProfiles/userpic.svg";
 
 const SidebarComponent = ({ userId }) => {
   const { logout } = useAuth();
@@ -25,7 +21,6 @@ const SidebarComponent = ({ userId }) => {
     min_preferred_age: "",
     images: null,
     combinedAge: "",
-
   });
 
   // console.log("MY user id in the sidebar", userId)
@@ -47,7 +42,6 @@ const SidebarComponent = ({ userId }) => {
 
     // setLoader(false)
     window.location.href = "/";
-
   };
 
   useEffect(() => {
@@ -66,7 +60,6 @@ const SidebarComponent = ({ userId }) => {
       window.removeEventListener("resize", handleResize);
     };
   }, [isInputFocused]);
-
 
   const toggleSidebar = () => {
     setIsSidebarOpen(!isSidebarOpen);
@@ -130,7 +123,6 @@ const SidebarComponent = ({ userId }) => {
       setIsInputFocused(false);
     }
 
-
     if (name === "preferAge") {
       let minAge, maxAge;
       if (formData.combinedAge === "above 30") {
@@ -157,7 +149,6 @@ const SidebarComponent = ({ userId }) => {
       setFormData((prevData) => ({ ...prevData, [name]: value }));
     }
   };
-
 
   const handleSubmit = async (e) => {
     setStartLoader(true);
@@ -270,22 +261,19 @@ const SidebarComponent = ({ userId }) => {
         </div>
       ) : (
         <aside
-          className={`fixed top-0 w-64  md:w-[20%] h-screen z-50  transition-transform ${isSidebarOpen || !isMobileOrTablet ? "" : "-translate-x-full"
-            }`}
+          className={`fixed top-0 w-64  md:w-[20%] h-screen z-50  transition-transform ${
+            isSidebarOpen || !isMobileOrTablet ? "" : "-translate-x-full"
+          }`}
           aria-label="Sidebar"
         >
-
           {/* <div className="h-full px-3 py-4 overflow-y-auto bg-gradient-to-r from-[#26011C] via-[#9E5500] to-[#26011C]"> */}
           <div
             className="h-full px-3 py-4 overflow-y-auto"
             style={{
-              background: "radial-gradient( rgba(158,85,0,1) 0%, rgba(158,85,0,1) 4%, rgba(38,1,28,1) 100%)"
+              background:
+                "radial-gradient( rgba(158,85,0,1) 0%, rgba(158,85,0,1) 4%, rgba(38,1,28,1) 100%)",
             }}
           >
-
-
-
-
             <div className="flex justify-end px-3 py-4">
               <button onClick={toggleSidebar}>
                 <svg
@@ -301,16 +289,15 @@ const SidebarComponent = ({ userId }) => {
             </div>
             <div
               className="mb-6 flex ml-2"
-            // style={{ marginTop: "-36.5px" }}
+              // style={{ marginTop: "-36.5px" }}
             >
-              <img
-                className="h-20 w-20 "
-                src={logo}
-                alt="Ddate logo"
-              />
+              <img className="h-20 w-20 " src={logo} alt="Ddate logo" />
             </div>
             <ul className="w-full text-center">
-              <li className=" mb-2 flex flex-row items-center rounded-full p-2  text-gray-900  dark:text-white hover:bg-yellow-700 dark:hover:bg-yellow-800  group">
+              <li
+                onClick={() => navigate("/Profile", { state: userId })}
+                className=" mb-2 flex flex-row items-center rounded-full p-2  text-gray-900  dark:text-white hover:bg-yellow-700 dark:hover:bg-yellow-800  group"
+              >
                 {formData.images ? (
                   <div className="pt-2">
                     <img
@@ -323,22 +310,30 @@ const SidebarComponent = ({ userId }) => {
                     />
                   </div>
                 ) : (
-                  <img
-                    src={chirag}
-                    alt="Profile"
-                    className="rounded-full h-12 w-12"
-
-                  />
+                  // <img
+                  //   src="https://via.placeholder.com/150"
+                  //   alt="Profile"
+                  //   className="rounded-full h-12 w-12"
+                  // />
+                  <svg
+                    className="h-12 w-12 text-gray-200"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                  >
+                    <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z" />
+                  </svg>
                 )}
-                <button
-                  onClick={() => navigate("/Profile", { state: userId })}
-                  className="block p-2 text-white text-sm rounded"
-                >
+                <button className="block p-2 text-white text-sm rounded">
                   <span>My Profile</span>
                 </button>
               </li>
 
-              <li className=" mb-2 flex flex-row items-center rounded-full p-2 text-gray-900  dark:text-white hover:bg-yellow-700 dark:hover:bg-yellow-800  group">
+              <li
+                onClick={() => navigate("/Notification", { state: userId })}
+                className=" mb-2 flex flex-row items-center rounded-full p-2 text-gray-900  dark:text-white hover:bg-yellow-700 dark:hover:bg-yellow-800  group"
+              >
                 <div className="relative mr-2">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -354,15 +349,15 @@ const SidebarComponent = ({ userId }) => {
                   </svg>
                   <span className="absolute top-0  right-0 w-2 h-2 bg-yellow-600 rounded-full"></span>
                 </div>
-                <button
-                  onClick={() => navigate("/Notification", { state: userId })}
-                  className="block p-2 text-white text-sm rounded"
-                >
+                <button className="block p-2 text-white text-sm rounded">
                   <span>Matches</span>
                 </button>
               </li>
 
-              <li className=" mb-2 flex flex-row items-center  p-2 text-gray-900 rounded-full dark:text-white hover:bg-yellow-700 dark:hover:bg-yellow-800  group">
+              <li
+                onClick={() => navigate("/ChattingPage", { state: userId })}
+                className=" mb-2 flex flex-row items-center  p-2 text-gray-900 rounded-full dark:text-white hover:bg-yellow-700 dark:hover:bg-yellow-800  group"
+              >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="24"
@@ -378,16 +373,16 @@ const SidebarComponent = ({ userId }) => {
                     fill="white"
                   />
                 </svg>
-                <button
-                  onClick={() => navigate("/ChattingPage")}
-                  className="block p-2 text-white text-sm rounded">
+                <button className="block p-2 text-white text-sm rounded">
                   <span>Messages</span>
                 </button>
               </li>
 
               <li
                 className=" mb-2 cursor-pointer flex flex-row items-center bg-yellow-600 py-1 rounded-full  "
-                onClick={() => { setShowDropdown(!showDropDown) }}
+                onClick={() => {
+                  setShowDropdown(!showDropDown);
+                }}
                 style={{
                   background:
                     "radial-gradient(68.18% 68.18% at 50% 50%, #FFC107 0%, #E28110 100%)",
@@ -431,8 +426,11 @@ const SidebarComponent = ({ userId }) => {
                 </div>
               </li>
             </ul>
-            {showDropDown &&
-              <form className="transition-all duration-500 ease-in-out " onSubmit={handleSubmit}>
+            {showDropDown && (
+              <form
+                className="transition-all duration-500 ease-in-out "
+                onSubmit={handleSubmit}
+              >
                 <div className="flex flex-col mb-2 ml-4 text-white">
                   <fieldset className="mb-1">
                     <legend className="font-bold p-2 text-base rounded">
@@ -528,17 +526,19 @@ const SidebarComponent = ({ userId }) => {
                         "radial-gradient(68.18% 68.18% at 50% 50%, #FFC107 0%, #E28110 100%)",
                     }}
                     onClick={() => {
-                      console.log("Form data by tushar jain" + formData.location);
-                      navigate("/Swipe", { state: { forceRerender: Date.now() } })
-                    }
-                    }
+                      console.log(
+                        "Form data by tushar jain" + formData.location
+                      );
+                      navigate("/Swipe", {
+                        state: { forceRerender: Date.now() },
+                      });
+                    }}
                   >
                     Apply
                   </button>
                 </div>
-
               </form>
-            }
+            )}
             <div className="flex flex-col items-center">
               <button
                 onClick={logoutHandler}
@@ -559,4 +559,3 @@ const SidebarComponent = ({ userId }) => {
 };
 
 export default SidebarComponent;
-
--- src/DDate_frontend/src/auth/useAuthClient.jsx
+++ src/DDate_frontend/src/auth/useAuthClient.jsx
@@ -1,48 +1,29 @@
-import React, { createContext, useContext, useEffect, useState } from 'react';
+import React, { createContext, useContext, useEffect, useState } from "react";
 import { AuthClient } from "@dfinity/auth-client";
-import { createActor } from '../../.././declarations/DDate_backend/index';
-// import Login from '../Auth/Login';
-// import Navbar from '../layouts/Navbar';
-import WalletModal from '../Components/WalletModal';
-import App from '../App';
-import HomePage from '../Pages/HomePage';
+import { createActor } from "../../../declarations/DDate_backend/index";
+import WalletModal from "../Components/WalletModal";
+import HomePage from "../Pages/HomePage";
 
 const AuthContext = createContext();
 
 const defaultOptions = {
-  /**
-   *  @type {import("@dfinity/auth-client").AuthClientCreateOptions}
-   */
   createOptions: {
-    // idleOptions: {
-    //   // Set to true if you do not want idle functionality
-    //   disableIdle: true,
-    // },
     idleOptions: {
-      idleTimeout: 1000 * 60 * 30, // set to 30 minutes
-      disableDefaultIdleCallback: true, // disable the default reload behavior
+      idleTimeout: 1000 * 60 * 30,
+      disableDefaultIdleCallback: true,
     },
   },
-  /**
-   * @type {import("@dfinity/auth-client").AuthClientLoginOptions}
-   */
   loginOptionsii: {
     identityProvider:
       process.env.DFX_NETWORK === "ic"
         ? "https://identity.ic0.app/#authorize"
         : `http://rdmx6-jaaaa-aaaaa-aaadq-cai.localhost:4943`,
-    // : `https://nfid.one/authenticate/?applicationName=my-ic-app#authorize`,
-    // :`https://nfid.one/authenticate/?applicationName=my-ic-app#authorize`
   },
   loginOptionsnfid: {
-    identityProvider:
-      process.env.DFX_NETWORK === "ic"
-        // ? "https://identity.ic0.app/#authorize"
-        // : `http://rdmx6-jaaaa-aaaaa-aaadq-cai.localhost:4943`,
-        ? `https://nfid.one/authenticate/?applicationName=my-ic-app#authorize`
-        : `https://nfid.one/authenticate/?applicationName=my-ic-app#authorize`
+    identityProvider: `https://nfid.one/authenticate/?applicationName=my-ic-app#authorize`,
   },
 };
+
 export const useAuthClient = (options = defaultOptions) => {
   const [authClient, setAuthClient] = useState(null);
   const [isAuthenticated, setIsAuthenticated] = useState(null);
@@ -50,74 +31,58 @@ export const useAuthClient = (options = defaultOptions) => {
   const [principal, setPrincipal] = useState(null);
   const [backendActor, setBackendActor] = useState(null);
   const [publicKey, setPublicKey] = useState(null);
-  const backendCanisterId =
-    process.env.CANISTER_ID_DDATE_BACKEND ||
-    process.env.CANISTER_ID_DDATE_BACKEND;
 
-  const frontendCanisterId =
-    process.env.CANISTER_ID_DDATE_FRONTEND ||
-    process.env.CANISTER_ID_DDATE_FRONTEND;
+  const backendCanisterId = process.env.CANISTER_ID_DDATE_BACKEND;
+  const frontendCanisterId = process.env.CANISTER_ID_DDATE_FRONTEND;
 
   useEffect(() => {
     AuthClient.create(options.createOptions).then((client) => {
       setAuthClient(client);
+      reloadLogin(client); // Call reloadLogin here
     });
   }, []);
 
-  // console.log('authClient',authClient)
   const login = async (val) => {
-    // console.log('val',val)
     return new Promise(async (resolve, reject) => {
       try {
-
-        // console.log("authClient.getIdentity().getPrincipal().isAnonymous() =>>>>>>>>>>",authClient.getIdentity().getPrincipal().isAnonymous());
         if (
           authClient.isAuthenticated() &&
-          (await authClient.getIdentity().getPrincipal().isAnonymous()) ===
-          false
+          !(await authClient.getIdentity().getPrincipal().isAnonymous())
         ) {
-
           clientInfo(authClient);
-          resolve(AuthClient);
+          resolve(authClient);
         } else {
-
-          // console.log("val=>>>.", val);
-          let opt = val === "ii" ? "loginOptionsii" : "loginOptionsnfid"
+          const opt = val === "ii" ? "loginOptionsii" : "loginOptionsnfid";
           authClient.login({
             ...options[opt],
-            onError: (error) => reject(error),
-            onSuccess: (val) => {
+            onError: reject,
+            onSuccess: () => {
               clientInfo(authClient);
               resolve(authClient);
             },
           });
         }
       } catch (error) {
-        console.log('error', error)
         reject(error);
       }
     });
   };
 
-  const reloadLogin = () => {
+  const reloadLogin = (client = authClient) => {
     return new Promise(async (resolve, reject) => {
       try {
         if (
-          authClient.isAuthenticated() &&
-          (await authClient.getIdentity().getPrincipal().isAnonymous()) ===
-          false
+          client.isAuthenticated() &&
+          !(await client.getIdentity().getPrincipal().isAnonymous())
         ) {
-          clientInfo(authClient);
-          resolve(AuthClient);
+          clientInfo(client);
+          resolve(client);
         }
       } catch (error) {
         reject(error);
       }
     });
   };
-
-
-
 
   const clientInfo = async (client) => {
     const isAuthenticated = await client.isAuthenticated();
@@ -125,51 +90,54 @@ export const useAuthClient = (options = defaultOptions) => {
     const principal = identity.getPrincipal();
     const publicKey = identity._delegation.publicKey;
 
-
-    // console.log("isAuthenticated",isAuthenticated,"principal",principal, "identity",identity );
     setAuthClient(client);
     setIsAuthenticated(isAuthenticated);
     setIdentity(identity);
     setPrincipal(principal);
     setPublicKey(publicKey);
-    let principalText = principal.toText();
 
-    // console.log("principalText???? ",principalText);
-    // localStorage.setItem("id",JSON.stringify(principalText))
-
-    if (isAuthenticated && identity && principal && principal.isAnonymous() === false) {
-      let backendActor = createActor(backendCanisterId, { agentOptions: { identity: identity, verifyQuerySignatures: false } });
-
-      // console.log("backendActor",backendActor);
+    if (isAuthenticated && identity && principal && !principal.isAnonymous()) {
+      const backendActor = createActor(backendCanisterId, {
+        agentOptions: { identity, verifyQuerySignatures: false },
+      });
       setBackendActor(backendActor);
     }
 
     return true;
-  }
+  };
+
   const logout = async () => {
     await authClient?.logout();
-  }
+  };
 
   return {
-    login, logout, authClient, isAuthenticated, identity, principal, frontendCanisterId, backendCanisterId, backendActor, reloadLogin, publicKey
+    login,
+    logout,
+    authClient,
+    isAuthenticated,
+    identity,
+    principal,
+    frontendCanisterId,
+    backendCanisterId,
+    backendActor,
+    reloadLogin,
+    publicKey,
   };
-}
+};
 
 export const AuthProvider = ({ children }) => {
   const auth = useAuthClient();
+
   if (!auth.isAuthenticated || !auth.backendActor) {
     return (
       <AuthContext.Provider value={auth}>
         <WalletModal />
         <HomePage />
       </AuthContext.Provider>
-    )
+    );
   }
-  return (
-    <AuthContext.Provider value={auth}>
-      {children}
-    </AuthContext.Provider>
-  )
+
+  return <AuthContext.Provider value={auth}>{children}</AuthContext.Provider>;
 };
 
 export const useAuth = () => useContext(AuthContext);
